8539855100a043f8b77a2b823e6fd794001a6474
apodosyan <podosyan.webpartner@gmail.com>
2015-12-16 12:02:01 +0400

    some structure changes

--- check-payment.php
+++ check-payment.php
@@ -1,82 +1,63 @@
 <?php
 
-$result = $wpdb->get_row("SELECT * FROM paypal_payments ", ARRAY_A);
-var_dump(sizeof($result));
-//$result = $wpdb->get_results("SELECT * FROM paypal_payments", ARRAY_A);
-var_dump($result);
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'paypal.com') > 0) {
 
-/*
-* Template Name: checking paypal payment
-*/
-$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$query_str = parse_url($actual_link, PHP_URL_QUERY);
-parse_str($query_str, $query_params);
+    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $query_str = parse_url($actual_link, PHP_URL_QUERY);
+    parse_str($query_str, $query_params);
 
-$log = fopen("logs/ipn.log", "a");
-fwrite($log, "\n\nipn - " . gmstrftime ("%b %d %Y %H:%M:%S", time()) . "\n");
-fwrite($log,"buyer_user_id"."=".$query_params['buyer_user_id']."\n");
+    $user_key = $query_params['buyer_user_id'];
 
-$req = "cmd=_notify-validate";
+    $log = fopen("logs/ipn.log", "a");
 
-foreach($_POST as $key=>$val)
-{
-    $req.= "&".$key."=".urlencode($val);
-    fwrite($log,$key."=".$val."\n");
-}
+    fwrite($log, "\n\nipn - " . gmstrftime ("%b %d %Y %H:%M:%S", time()) . "\n");
 
-$header = "POST http://www.paypal.com/cgi-bin/webscr HTTP/1.0\r\n";
-$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-$header .= "Content-Length: " . strlen ($req) . "\r\n\r\n";
-$fp = fsockopen ("www.sandbox.paypal.com", 80, $errno, $errstr, 30);
+    if($user_key) {
 
-if (!$fp)
-{
-    echo "$errstr ($errno)";
-    fwrite($log, "Failed to open HTTP connection!\n");
-    fwrite($log, $errstr." ".$errno);
-    fclose ($log);
-    return;
-}
+        fwrite($log,"buyer_user_id"."=".$user_key."\n");
+        $req = "cmd=_notify-validate";
 
-fputs ($fp, $header . $req);
+        foreach($_POST as $key=>$val)
+        {
+            $req.= "&".$key."=".urlencode($val);
+            fwrite($log,$key."=".$val."\n");
+        }
 
-$res="";
-while (!feof($fp))
-    $res .= fgets ($fp, 1024);
-fclose ($fp);
+        if ($_POST["payment_status"] == "Completed")
+        {
 
-if (strpos($res, "VERIFIED")===FALSE)
-{
-    fwrite($log,"ERROR - UnVERIFIIED payment\r\nPayPal response:");
-    fwrite($log,$res);
-    fclose($log);
-    return;
-}
 
-fwrite($log,"payment VERIFIIED\r\n");
+        }
 
-if ($_POST["payment_status"]!="Completed")
-{
-    if ($_POST["payment_status"]=="Pending" )
-    {
-        fwrite($log,"ERROR - payment status is not Completed - $_POST[payment_status] | $_POST[pending_reason]\r\n");
-        fclose($log);
-// а тут отмечаем заказ как оплаченный, но требующий подтверждение оплаты со стороны плательщика
-// такое бывает редко, но все же бывает и лучше подстраховаться.
+/*===========================================================================================*/
 
-        return;
+        if ($_POST["payment_status"] != "Completed")
+        {
+            if ($_POST["payment_status"]=="Pending" )
+            {
+                fwrite($log,"ERROR - payment status is not Completed - $_POST[payment_status] | $_POST[pending_reason]\r\n");
+                fclose($log);
+                // а тут отмечаем заказ как оплаченный, но требующий подтверждение оплаты со стороны плательщика
+                // такое бывает редко, но все же бывает и лучше подстраховаться.
+                die;
+
+            }
+
+            fwrite($log,"ERROR - payment status is not Completed - $_POST[payment_status] | $_POST[pending_reason]\r\n");
+            fclose($log);
+            //update order status
+            die;
+
+        }
+
+        fwrite($log,"OK - payment received $_POST[item_number].\r\n");
+
+            // тут отмечаем заказ оплаченным.
+            // деньги уже на счету продавца
+
     }
-
-    fwrite($log,"ERROR - payment status is not Completed - $_POST[payment_status] | $_POST[pending_reason]\r\n");
+    fwrite($log, 'pageEnd');
     fclose($log);
-    return;
-    //update order status
-
 }
-
-fwrite($log,"OK - payment received $_POST[item_number].\r\n");
-fclose($log);
-// тут отмечаем заказ оплаченным.
-// деньги уже на счету продавца
 
 ?>
--- form-paypal.php
+++ form-paypal.php
@@ -1,15 +1,15 @@
-    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
-        <input name="cmd" type="hidden" value="_xclick" />
-        <input name="business" type="hidden" value="maksdev0-facilitator@gmail.com" />
-        <input name="item_name" type="hidden" value="Car. Order #3" />
-        <input name="item_number" type="hidden" value="1" />
-        <input name="amount" type="hidden" value="50" />
-        <input name="no_shipping" type="hidden" value="1" />
-        <input name="rm" type="hidden" value="2" />
-        <input name="return" type="hidden" value="http://akezo.ru/wordpress/success-payment/" />
-        <input name="cancel_return" type="hidden" value="http://akezo.ru/wordpress/cancel-payment/" />
-        <input name="currency_code" type="hidden" value="USD" />
-        <input id='id' name="notify_url" type="hidden" value="" />
+	<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+		<input name="cmd" type="hidden" value="_xclick" />
+		<input name="business" type="hidden" value="your-account-name-in-paypal@gmail.com" />
+		<input name="item_name" type="hidden" value="Car. Order #3" />
+		<input name="item_number" type="hidden" value="1" />
+		<input name="amount" type="hidden" value="50" />
+		<input name="no_shipping" type="hidden" value="1" />
+		<input name="rm" type="hidden" value="2" />
+		<input name="return" type="hidden" value="http://yourSiteName.com/success-payment/" />
+		<input name="cancel_return" type="hidden" value="http://yourSiteName.com/cancel-payment/" />
+		<input name="currency_code" type="hidden" value="USD" />
+		<input id='id' name="notify_url" type="hidden" value="http://yourSiteName.com/check-payment/?buyer_user_id=xxxxxxxxxxx" />
 		<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
 		<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-    </form>
+	</form>
